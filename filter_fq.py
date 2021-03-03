adf783f079c7195cc7261bf8791977862e309747
Kun <tianguolangzi@gmail.com>
2021-03-03 22:51:40 +0800

    Update filter_fq.py

--- filter_fq.py
+++ filter_fq.py
@@ -1,27 +1,18 @@
-#!/public/home/jcli/public/bin/python3
-#-*-coding=utf-8 -*-
-##############################################################################
-#                  author     zhangkun                                       #
-#                  email      tianguolangzi@gmail.com                        #
-#                  V          2.0                                            #
-#                  date       2016-11-28                                      #
-##############################################################################
+#-*-coding=utf-8 -*-#
 import os,sys,gzip
 import time
 import argparse
 from collections import defaultdict
 from multiprocessing import Manager,Pool
-__version__=2.0
+__version__=1.0
 
-def purpose():
-    print('''因为使用cutadapt -u 的时候造成fq1中一条read是好的fq2的相应的read因为低质量值被完全去除只剩下read name ,read seq 为空,比对的时候软件不能识别''')
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--version', action = 'version', version = 'filter_fq.py {}'.format(__version__))
     parser.add_argument('fq1',  help = '' )
     parser.add_argument('fq2',  help = '' )
-    parser.add_argument('-t', '--cpu', type = int, default=20, help = '开启进程的数目,要视内存开启')
+    parser.add_argument('-t', '--cpu', type = int, default=20, help = '')
     return parser
 
 def readfile(File):
@@ -62,14 +53,9 @@ def writefile(j,fq1Name,fq2Name):
 
     
 def start(cpu,fq1,fq2,AA):
-    #fq1Read=readfile(args.fq1)
-    #fq2Read=readfile(args.fq2)
-    #if len(fq1Read) != len(fq2Read) or len(fq1Read)%4 != 0:
-    #    print("fq1和fq2 reads数目不等,或者数目不为的倍数!")
-    #    sys.exit(1)
+
     pool=Pool(processes=cpu)
     #length=len(fq1Read)
-    #print('总数:',length )
     #Num=int(length//4000000)+1
     print(cpu,fq1,fq2)
     C1='cat '
@@ -100,15 +86,15 @@ def start(cpu,fq1,fq2,AA):
             pool.apply_async(writefile, (n,fq1Name,fq2Name))
     pool.close()
     pool.join()
-    command1 =  C2 +" > " +fq2 +".passed"+ " && " +N1+" > "+fq1 +".nopassed"+" && " +N2 +" > "+ fq2+".nopassed"+ " && " + C1 +" > "+fq1 +".passed"  #必须使用&&,避免没有执行完就被删除
+    command1 =  C2 +" > " +fq2 +".passed"+ " && " +N1+" > "+fq1 +".nopassed"+" && " +N2 +" > "+ fq2+".nopassed"+ " && " + C1 +" > "+fq1 +".passed"  
     print(command1)
     if not os.system(command1):
         command2=Rm
         if not os.system(command2):
             print("merger is OVER")
-            return 0 #0代表完成
+            return 0 
 
-def get_canshu():
+def work():
     parser = get_parser()
     args = parser.parse_args()
     cpu=args.cpu
@@ -116,7 +102,7 @@ def get_canshu():
     fq2=args.fq2
     start(cpu,fq1,fq2)
 if __name__=='__main__':
-    get_canshu()
+    work()
 
 
 
